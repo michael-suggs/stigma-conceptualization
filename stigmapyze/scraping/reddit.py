10e3851dab0b599344a5b3547481f61a073e7c84
Michael Suggs <suggs.michael.jeffrey@gmail.com>
2021-01-24 15:04:03 -0500

    Work on reddit.py for parsing subreddit posts and comments.

--- stigmapyze/scraping/reddit.py
+++ stigmapyze/scraping/reddit.py
@@ -1,8 +1,11 @@
+import datetime as dt
 import json
 from collections import namedtuple
-from typing import Union
+from functools import singledispatch
+from typing import List, Union
 
 import praw
+from praw.models.comment_forest import CommentForest, Comment
 
 
 @dataclass(frozen=True, init=False)
@@ -29,14 +32,48 @@ class Config:
         self.redirect_uri = 'http://localhost:8080'
 
 
-def connect_and_configure(config_file: str):
+@dataclass
+class RedditPost:
+    id: str
+    title: str
+    text: str
+    comments: CommentForest
+
+
+def connect_and_configure(config_file: str) -> praw.Reddit:
     config = Config(config_file)
     reddit_conn = praw.Reddit(
         client_id=f'{config.client_id}',
         client_secret=f'{config.client_secret}',
-        user_agent=f'{config.user_agent}',
-        redirect_uri=f'{config.redirect_uri}'
+        user_agent=f'{config.user_agent}'
     )
 
+    return reddit_conn
 
 
+@singledispatch
+def parse_subreddit(
+    post_limit: int,
+    reddit_conn: praw.Reddit,
+    sub_name: str = 'SuicideWatch'
+):
+    posts: List[RedditPost] = []
+    subreddit = reddit_conn.subreddit(sub_name)
+
+    for post in subreddit.new(limit=post_limit):
+        posts.append(RedditPost(
+            id = post.id,
+            title = post.title,
+            text = post.selftext,
+            comments = post.comments
+        ))
+
+
+
+@parse_subreddit.register
+def _(
+    post_limit: dt.timedelta,
+    reddit_conn: praw.Reddit,
+    sub_name: str = 'SuicideWatch'
+):
+    ...
