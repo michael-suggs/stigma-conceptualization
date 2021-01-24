df9e82ed155f7da748505720825ebb35ec5fb219
Michael Suggs <suggs.michael.jeffrey@gmail.com>
2021-01-24 18:20:44 -0500

    Added documentation, post stringification, and post writing.

--- stigmapyze/common/reddit_post.py
+++ stigmapyze/common/reddit_post.py
@@ -1,12 +1,21 @@
 import json
 from dataclasses import dataclass
-from typing import Generator, List, Tuple
+from typing import Generator, List, TextIo, Tuple
 
 from praw.models.comment_forest import Comment, CommentForest
 
 
 @dataclass
 class RedditPost:
+    """Class holding info parsed from an individual Reddit post.
+
+    Attributes:
+        id (str): The post's ID (unique; defined by Reddit).
+        title (str): The post's title.
+        text (str): Body text of the original post.
+        comments (CommentForest): A praw CommentForest instance containing
+            all top-level comments and their replies.
+    """
     id: str
     title: str
     text: str
@@ -29,8 +38,42 @@ class RedditPost:
         }, indent=4)
 
     def get_top_level_comments(self) -> Generator[Tuple[str, str]]:
+        """Yields all top-level comments for this post.
+
+        Yields:
+            Generator[Tuple[str, str]]: A generator object returning tuples
+            in the form of (id, body) where "id" refers to the comment's
+            unique identifier string and "body" contains the body text of the
+            comment itself.
+        """
         for comment in self.comments:
             yield (comment.id, comment.body)
 
     def get_flattened_comments(self) -> List[Comment]:
+        """Flattens all comments and replies into a list.
+
+        Returns:
+            List[Comment]: A list of praw Comment objects in order. Comments
+            are flattened so that all replies to comments (both top-level and
+            nested) are present in a single-dimensional list.
+        """
         return self.comments.list()
+
+    def write_post(self, fp: TextIo, top_level_only: bool = False) -> None:
+        """Writes a post to a given file.
+
+        Parameters
+        ----------
+        fp : TextIo
+            A pointer to the file to write (json dumps) to.
+        top_level_only : bool, optional
+            If True, only writes the top-level comments. By default False.
+        """
+        comments = (list(self.get_top_level_comments()) if top_level_only
+            else self.get_flattened_comments())
+        json.dump(fp, {
+            'id': self.id,
+            'title': self.title,
+            'text': self.text,
+            'comments': comments
+        }, indent=4)
--- stigmapyze/scraping/reddit.py
+++ stigmapyze/scraping/reddit.py
@@ -34,6 +34,14 @@ class Config:
 
 
 def connect_and_configure(config_file: str) -> praw.Reddit:
+    """Creates a praw connection using info from a config.json file.
+
+    Args:
+        config_file (str): Path to the Reddit (praw) configuration file.
+
+    Returns:
+        praw.Reddit: A connected Reddit instance.
+    """
     config = Config(config_file)
     reddit_conn = praw.Reddit(
         client_id=f'{config.client_id}',
@@ -50,6 +58,18 @@ def parse_subreddit(
     reddit_conn: praw.Reddit,
     sub_name: str = 'SuicideWatch'
 ) -> List[RedditPost]:
+    """Parses a desired number of posts and comments from a given subreddit.
+
+    Args:
+        post_limit (int): Maximum number of posts to parse.
+        reddit_conn (praw.Reddit): The praw Reddit connection to use.
+        sub_name (str, optional): The subreddit to parse.
+            Defaults to 'SuicideWatch'.
+
+    Returns:
+        List[RedditPost]: A list of parsed post objects containing the title,
+            text, and the post's CommentForest object containing all comments.
+    """
     subreddit = reddit_conn.subreddit(sub_name)
     posts: List[RedditPost] = []
 
@@ -70,7 +90,26 @@ def _(
     post_limit: dt.timedelta,
     reddit_conn: praw.Reddit,
     sub_name: str = 'SuicideWatch'
-):
+) -> List[RedditPost]:
+    """Dispatched method for `parse_subreddit` with a timedelta instead.
+
+    Parses posts (and their respective comments) from within a given timeframe
+    (timedelta) from the current time. The number of posts parsed will thus be
+    dependent on the activity level of the subreddit accessed--subreddits with
+    more activity will return a higher number of posts for a given timeframe
+    than those with less.
+
+    Args:
+        post_limit (dt.timedelta): Time to subtract from the present moment to
+            determine the oldest posts to parse.
+        reddit_conn (praw.Reddit): The praw Reddit connection to use.
+        sub_name (str, optional): The subreddit to parse.
+            Defaults to 'SuicideWatch'.
+
+    Returns:
+        List[RedditPost]: A list of parsed post objects containing the title,
+            text, and the post's CommentForest object containing all comments.
+    """
     subreddit = reddit_conn.subreddit(sub_name)
     posts: List[RedditPost] = []
     post_limit = (
